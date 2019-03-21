ae2cbe85542c4c5fe137ae6c79d036b97985a8e8
alejandra cantu <alejandra.cantu13@gmail.com>
2019-03-21 22:29:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,25 @@
 class Dog
 
-def initialize
-@name = name_of_dog
-end
+  def initialize
 
-def name=(name_of_dog = "Mutt")
-@name = name_of_dog
+  end
+#setter - writer
+  def breed=(this_dog_breed = "Mutt")
+    @breed = this_dog_breed
+
+  end
+#getter-reader
+  def breed
+    @breed
+  end
+
+  def name=(this_dog_name)
+    @name = this_dog_name
+
 end
 
 def name
   @name
-
 end
-
 
 end
